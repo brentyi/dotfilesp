db99ab300f721763a2ba4a46c15f57e63d783667
Brent Yi <yibrenth@gmail.com>
2017-03-12 11:11:17 -0700

    use https/github for gpg key retrieval

--- scripts/install_ros_desktop.sh
+++ scripts/install_ros_desktop.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+# sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install ros-indigo-desktop-full -y
 sudo rosdep init
