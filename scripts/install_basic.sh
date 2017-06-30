24f3d81df00286597bf36d9c1f45ccfd73aaf5d5
Brent Yi <yibrenth@gmail.com>
2017-06-30 18:47:06 +0000

    install - automating pip update

--- scripts/install_basic.sh
+++ scripts/install_basic.sh
@@ -6,6 +6,8 @@ sudo apt-get update
 
 # python stuff
 sudo apt-get install python-dev python-pip python3-dev python3-pip -y
+sudo pip2 install --upgrade pip
+sudo pip3 install --upgrade pip
 
 # install common utilities
 sudo apt-get install neovim tmux htop build-essential xclip -y
