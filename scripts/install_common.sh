94f94b9b0e30880c0b7d10acff14b4b941735c27
Brent Yi <yibrenth@gmail.com>
2019-12-11 17:52:03 -0800

    setup - add fedora support

--- scripts/install_common.sh
+++ scripts/install_common.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# update repositories
-sudo apt-get update
+if [[ `lsb_release -is` == "Ubuntu" ]]; then
+    # update repositories
+    sudo apt-get update
 
-# python stuff
-sudo apt-get install python-dev python-pip python3-dev python3-pip -y
-sudo pip2 install --upgrade pip
-sudo pip3 install --upgrade pip
+    # python stuff
+    sudo apt-get install python-dev python-pip python3-dev python3-pip -y
+    sudo pip2 install --upgrade pip
+    sudo pip3 install --upgrade pip
 
-# install common utilities
-sudo apt-get install vim ctags tmux htop build-essential xclip bc -y
+    # install common utilities
+    sudo apt-get install vim ctags tmux htop build-essential xclip bc -y
+
+elif [[ `lsb_release -is` == "Fedora" ]]; then
+    # install common utilities
+    sudo dnf install vim ctags tmux htop xclip -y
+fi
--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # set up zsh
-sudo apt-get install zsh -y
+if [[ `lsb_release -is` == "Ubuntu" ]]; then
+    sudo apt-get install zsh -y
+elif [[ `lsb_release -is` == "Fedora" ]]; then
+    sudo dnf install zsh -y
+fi
 chsh -s /bin/zsh
