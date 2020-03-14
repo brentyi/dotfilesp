c485ae228b56e928f00b975c2365b8d6a98ca775
Brent Yi <yibrenth@gmail.com>
2020-03-13 17:55:56 -0700

    jupyter - add some bindings

--- common/.jupyter/custom/custom.js
+++ common/.jupyter/custom/custom.js
@@ -31,3 +31,21 @@ require(
         });
       }, 1000);
     });
+
+// Configure Jupyter Keymap
+require(
+    [
+      'nbextensions/vim_binding/vim_binding',
+      'base/js/namespace',
+    ],
+    function(vim_binding, ns) {
+      // Add post callback
+      vim_binding.on_ready_callbacks.push(function() {
+        var km = ns.keyboard_manager;
+        // Allow Ctrl-2 to change the cell mode into Markdown in Vim normal mode
+        km.edit_shortcuts.add_shortcut(
+            'ctrl-2', 'vim-binding:change-cell-to-markdown', true);
+        // Update Help
+        km.edit_shortcuts.events.trigger('rebuild.QuickHelp');
+      });
+    });
