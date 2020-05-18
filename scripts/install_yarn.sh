1dc0c5c1819e287b04ee314f44b6f07a63719918
Brent Yi <yibrenth@gmail.com>
2020-05-18 05:57:29 -0700

    setup - automatically add yarn bin to path

--- scripts/install_yarn.sh
+++ scripts/install_yarn.sh
@@ -5,3 +5,6 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
 sudo apt-get install yarn
+
+echo "PATH=\"\$PATH:`yarn global bin`\"" >> ~/.bashrc
+echo "PATH=\"\$PATH:`yarn global bin`\"" >> ~/.zshrc
