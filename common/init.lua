83e34cfa13b9d7ba02cc4a8807306eea396f9892
Brent Yi <yibrenth@gmail.com>
2024-09-21 01:14:16 +0000

    vim - fix Trouble, remove icons

--- common/init.lua
+++ common/init.lua
@@ -560,7 +560,7 @@ local lazy_plugins = {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			require("lspconfig").pyright.setup({ capabilities = capabilities })
 			require("lspconfig").lua_ls.setup({ capabilities = capabilities })
-			require("lspconfig").tsserver.setup({ capabilities = capabilities })
+			require("lspconfig").ts_ls.setup({ capabilities = capabilities })
 			require("lspconfig").html.setup({ capabilities = capabilities })
 			require("lspconfig").cssls.setup({ capabilities = capabilities })
 			require("lspconfig").eslint.setup({ capabilities = capabilities })
@@ -621,59 +621,46 @@ local lazy_plugins = {
 	},
 	{
 		"folke/trouble.nvim",
-		config = function()
-			vim.keymap.set("n", "<Leader><Tab>", ":TroubleToggle<CR>", {})
-			require("trouble").setup({
-				position = "bottom", -- position of the list can be: bottom, top, left, right
-				height = 10, -- height of the trouble list when position is top or bottom
-				width = 50, -- width of the list when position is left or right
-				icons = false, -- use devicons for filenames
-				mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
-				fold_open = "v", -- icon used for open folds
-				fold_closed = ">", -- icon used for closed folds
-				group = true, -- group results by file
-				padding = true, -- add an extra new line on top of the list
-				action_keys = { -- key mappings for actions in the trouble list
-					-- map to {} to remove a mapping, for example:
-					-- close = {},
-					close = "q", -- close the list
-					cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
-					refresh = "r", -- manually refresh
-					jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
-					open_split = { "<c-x>" }, -- open buffer in new split
-					open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
-					open_tab = { "<c-t>" }, -- open buffer in new tab
-					jump_close = { "o" }, -- jump to the diagnostic and close the list
-					toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
-					toggle_preview = "P", -- toggle auto_preview
-					hover = "K", -- opens a small popup with the full multiline message
-					preview = "p", -- preview the diagnostic location
-					close_folds = { "zM", "zm" }, -- close all folds
-					open_folds = { "zR", "zr" }, -- open all folds
-					toggle_fold = { "zA", "za" }, -- toggle fold of current file
-					previous = "k", -- previous item
-					next = "j", -- next item
-				},
-				indent_lines = true, -- add an indent guide below the fold icons
-				auto_open = false, -- automatically open the list when you have diagnostics
-				auto_close = false, -- automatically close the list when you have no diagnostics
-				auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
-				auto_fold = false, -- automatically fold a file trouble list at creation
-				auto_jump = { "lsp_definitions" }, -- for the given modes, automatically jump if there is only a single result
-				signs = {
-					-- icons / text used for a diagnostic
-					error = "error",
-					warning = "warn ",
-					hint = "hint ",
-					information = "info ",
-				},
-				use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
-			})
-		end,
+		cmd = "Trouble",
+		opts = {},
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
 	},
 	{
 		"yetone/avante.nvim",
 		event = "VeryLazy",
+		lazy = false,
+		version = false,
 		opts = {
 			windows = {
 				sidebar_header = {
@@ -682,19 +669,12 @@ local lazy_plugins = {
 				},
 			},
 		},
+		build = "make",
 		dependencies = {
-			"nvim-tree/nvim-web-devicons",
+			-- "nvim-tree/nvim-web-devicons",
 			"stevearc/dressing.nvim",
 			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
-			--- The below is optional, make sure to setup it properly if you have lazy=true
-			-- {
-			-- 	"MeanderingProgrammer/render-markdown.nvim",
-			-- 	opts = {
-			-- 		file_types = { "markdown", "Avante" },
-			-- 	},
-			-- 	ft = { "markdown", "Avante" },
-			-- },
 		},
 	},
 	{
