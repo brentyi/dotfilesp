2ca317fb5ed325fb9babdf70c3c960fb87c7df23
Brent Yi <yibrenth@gmail.com>
2025-04-10 17:15:12 -0700

    setup - bump npm to 20.x

--- ubuntu/setup/install_npm.sh
+++ ubuntu/setup/install_npm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt install curl
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt update
 sudo apt install nodejs -y
