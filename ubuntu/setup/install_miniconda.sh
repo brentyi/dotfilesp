0089dcca5e89e440a733750812f788d3aebc5857
Brent Yi <yibren@amazon.com>
2025-11-05 01:30:31 -0800

    setup - miniconda MacOS

--- ubuntu/setup/install_miniconda.sh
+++ ubuntu/setup/install_miniconda.sh
@@ -18,6 +18,16 @@ if [[ `uname -m` == "aarch64" ]]; then # Miniforge
     echo "PATH=\$PATH:$destination/miniforge/bin" >> ~/.zshrc
     rm ./install_conda.sh
 
+elif [[ `uname -m` == "arm64" ]]; then
+    echo "Installing Miniconda (OSX / arm64) to: $destination"
+
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -O ./install_conda.sh
+    bash ./install_conda.sh -b -p $destination/miniconda
+
+    echo "PATH=\$PATH:$destination/miniconda/bin" >> ~/.bashrc
+    echo "PATH=\$PATH:$destination/miniconda/bin" >> ~/.zshrc
+    rm ./install_conda.sh
+
 else # Miniconda 64bit
     echo "Installing Miniconda to: $destination"
 
